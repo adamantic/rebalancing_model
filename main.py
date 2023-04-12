b3f275d621b1888f1683e25906bf484f9206fc61
Michael Frazis <michaelfrazis@gmail.com>
2023-04-12 21:33:45 +1000

    Making changes

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ import numpy as np
 import datetime as dt
 
 today = dt.date.today()
-top = '2021-12-01'
 
 # Initialize variables
-stock1 = 'QQQ'
+stock1 = 'TQQQ'
 stock2 = 'BIL'
-weight1 = 0.99
+weight1 = 0.5
 start_date = pd.Timestamp('2000-03-01')
 end_date = pd.Timestamp(today)
 
@@ -20,7 +19,6 @@ rebalance_period = 5  # Number of weekdays between rebalancing
 
 tickers = [stock1, stock2]
 portfolio_weights = {stock1: weight1, stock2: weight2}
-
 
 # Download historical data for tickers
 ohlc_data = yf.download(tickers, start_date, end_date)['Close']
@@ -54,33 +52,73 @@ portfolio_value_df = pd.DataFrame(portfolio_value, index=ohlc_data.index, column
 stock1_returns = (1 + ohlc_data[stock1].pct_change()).cumprod()
 stock1_normalized = initial_value * stock1_returns
 
-# Calculate CAGR
+# Calculate Stock1 stats
+stock1_normalized = stock1_normalized.iloc[1:]  # Drop the first row to avoid division by zero
+
+
+
+# CAGR
+stock1_years = (stock1_normalized.index[-1] - stock1_normalized.index[0]).days / 365
+stock1_CAGR = (stock1_normalized.iloc[-1] / stock1_normalized.iloc[0]) ** (1 / stock1_years) - 1
+
+# Max Drawdown
+stock1_normalized_df = pd.DataFrame(stock1_normalized)
+stock1_normalized_df['max'] = stock1_normalized_df[stock1].cummax()
+stock1_normalized_df['drawdown'] = (stock1_normalized_df[stock1] - stock1_normalized_df['max']) / stock1_normalized_df['max']
+stock1_max_drawdown = stock1_normalized_df['drawdown'].min()
+
+# Volatility (annualized)
+stock1_normalized_df['daily_return'] = stock1_normalized_df[stock1].pct_change()
+stock1_volatility = stock1_normalized_df['daily_return'].std() * np.sqrt(252)
+
+# Sharpe Ratio (assuming risk-free rate of 0)
+stock1_sharpe_ratio = (stock1_CAGR - 0) / stock1_volatility
+
+print(stock1, "Stats")
+print("Total Return: ")
+print("CAGR:", stock1_CAGR)
+print("Max Drawdown:", stock1_max_drawdown)
+print("Volatility:", stock1_volatility)
+print("Sharpe Ratio:", stock1_sharpe_ratio)
+
+
+# Calculate stats for the portfolio
+
+# Calculate total return for the portfolio
+initial_portfolio_value = portfolio_value_df.iloc[0, 0]
+ending_portfolio_value = portfolio_value_df.iloc[-1, 0]
+total_return = (ending_portfolio_value / initial_portfolio_value) - 1
+
+
+# CAGR
 years = (portfolio_value_df.index[-1] - portfolio_value_df.index[0]).days / 365
 CAGR = (portfolio_value_df.iloc[-1] / portfolio_value_df.iloc[0]) ** (1 / years) - 1
 
-# Calculate max drawdown
+# Max Drawdown
 portfolio_value_df['max'] = portfolio_value_df['Portfolio Value'].cummax()
 portfolio_value_df['drawdown'] = (portfolio_value_df['Portfolio Value'] - portfolio_value_df['max']) / portfolio_value_df['max']
 max_drawdown = portfolio_value_df['drawdown'].min()
 
-# Calculate volatility (annualized)
+# Volatility (annualized)
 portfolio_value_df['daily_return'] = portfolio_value_df['Portfolio Value'].pct_change()
 volatility = portfolio_value_df['daily_return'].std() * np.sqrt(252)
 
-# Calculate Sharpe ratio (assuming risk-free rate of 0)
+# Sharpe Ratio (assuming risk-free rate of 0)
 sharpe_ratio = (CAGR - 0) / volatility
 
+print("\nPortfolio Stats")
+print("Total Return:", total_return)
 print("CAGR:", CAGR[0])
 print("Max Drawdown:", max_drawdown)
 print("Volatility:", volatility)
 print("Sharpe Ratio:", sharpe_ratio)
 
-
 # Plot the portfolio value and the amounts of the two tickers
 fig, ax = plt.subplots(figsize=(10, 6))
 ax.plot(portfolio_value_df.index, portfolio_value_df['Portfolio Value'], label='Portfolio Value')
-#ax.plot(stock1_normalized.index, stock1_normalized, label=stock1)  # This line plots stock1_normalized
+ax.plot(stock1_normalized.index, stock1_normalized, label=stock1)  # This line plots stock1_normalized
 ax.set_xlabel('Date')
 ax.set_ylabel('Price')
 ax.legend()
 plt.show()
+
